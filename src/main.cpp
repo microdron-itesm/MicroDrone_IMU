e77dde3fcc29f2a8b08defabc446f4e03fcca2f7
Abiel <empoleom@gmail.com>
2020-11-23 21:20:44 -0600

    Calibration testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <queue>
 #include <Arduino.h>
 #include <Wire.h>
-#include <SparkFunMPU9250-DMP.h>
+#include <MPU9250.h>
 #include <common/mavlink.h>
-#include "state_estimator.h"
 
-MPU9250_DMP imu;
+extern "C"{
+#include "MadgwickAHRS.h"
+}
+
+MPU9250 imu(Wire, 0x68);
 volatile ssize_t cts = 0;
 volatile bool imuReady = false;
+
+const float radsToDegs = (180.0f / M_PI);
+
+bool calibrated = false;
+
+const bool enableMotionCal = true;
 
 std::queue<mavlink_message_t> msgBuffer;
 uint8_t msgByteBuffer[MAVLINK_MAX_PACKET_LEN + sizeof(uint64_t)];
@@ -20,7 +29,6 @@ float acc[3];
 float gyro[3];
 float mag[3];
 
-float q[4];
 float euler_angles[3];
 
 unsigned long lastImuTime = 0;
@@ -30,8 +38,6 @@ void initIMUValues(){
   memset(gyro, 0, sizeof(gyro));
   memset(mag, 0, sizeof(mag));
 
-  memset(q, 0, sizeof(q));
-  q[0] = 1.0;
   memset(euler_angles, 0, sizeof(euler_angles));
   lastImuTime = micros();
 }
@@ -54,35 +60,39 @@ void setup() {
   Serial1.begin(115200);
   SerialUSB.begin(115200);
   while(!SerialUSB);
-  SerialUSB.println("hello there");
+  //SerialUSB.println("hello there");
 
   memset(msgByteBuffer, 0, sizeof(msgByteBuffer));
 
   int status = imu.begin();
-  imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
-  imu.setSampleRate(1000); //Use 1000hz update rate
-  imu.setCompassSampleRate(100);
+  imu.setSrd(9); //Use 100hz update rate
 
-  imu.setAccelFSR(4);
-  imu.setGyroFSR(1000);
-  imu.setLPF(98);
-
-  imu.enableInterrupt();
-  imu.setIntLevel(INT_ACTIVE_LOW); //Active_low interrupt
-  imu.setIntLatched(INT_LATCHED);
+  imu.setAccelRange(MPU9250::ACCEL_RANGE_4G);
+  imu.setGyroRange(MPU9250::GYRO_RANGE_1000DPS);
+  imu.setDlpfBandwidth(MPU9250::DLPF_BANDWIDTH_41HZ);
 
   initIMUValues();
 
-  if(imu.selfTest() != 0x7){
-    //Self test failed
-    //Die
-    for(;;){
-        Serial1.write(0xDE); Serial1.write(0xAD);
-    }
-  }
-
   pinMode(13, INPUT);
   attachInterrupt(digitalPinToInterrupt(13), picInterrupt, FALLING);
+
+  status = imu.calibrateGyro();
+
+  if(status < 0){
+    //Die
+    while(1);
+  }
+
+
+  status = imu.calibrateAccel();
+  if(status < 0){
+    //Die
+    while(1);
+  }
+  
+  imu.setMagCalX(-13.47f, 0.997f);
+  imu.setMagCalY(-8.17f, 1.021f);
+  imu.setMagCalZ(-18.98f, 0.9783f);
 
   lastHb = millis();
 }
@@ -90,41 +100,66 @@ void setup() {
 void loop() {
   mavlink_message_t msg;
 
-  if(true){
-    imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
+  if(millis() - lastImuTime > 2){ //500hz
+    imu.readSensor();
     imuReady = false;
 
-    acc[0] = imu.calcAccel(imu.ax);
-    acc[1] = imu.calcAccel(imu.ay);
-    acc[2] = imu.calcAccel(imu.az);
+    acc[0] = imu.getAccelX_mss();//m/s/s
+    acc[1] = imu.getAccelY_mss();
+    acc[2] = imu.getAccelZ_mss();
 
-    SerialUSB.print(acc[0]);
+    gyro[0] = imu.getGyroX_rads();//rads
+    gyro[1] = imu.getGyroY_rads();
+    gyro[2] = imu.getGyroZ_rads();
 
-    gyro[0] = imu.calcGyro(imu.gx);
-    gyro[1] = imu.calcGyro(imu.gy);
-    gyro[2] = imu.calcGyro(imu.gz);
+    mag[0] = imu.getMagX_uT();//uT
+    mag[1] = imu.getMagY_uT();
+    mag[2] = imu.getMagZ_uT();
 
-    mag[0] = imu.calcMag(imu.mx);
-    mag[1] = imu.calcMag(imu.my);
-    mag[2] = imu.calcMag(imu.mz);
-
-    float dt = ((micros() - lastImuTime) / 1000000.0f);
-
-    UpdateState(acc, gyro, mag, q, dt, euler_angles);
-    lastImuTime = micros();
-
-    mavlink_msg_attitude_quaternion_pack(1, MAV_COMP_ID_IMU, &msg, millis(), q[0], q[1], q[2], q[3], gyro[0], gyro[1], gyro[2]);
-    msgBuffer.emplace(msg);
     
-    SerialUSB.print("Roll: ");
-    SerialUSB.print(euler_angles[0]);
-    SerialUSB.print(" Pitch: ");
-    SerialUSB.print(euler_angles[1]);
-    SerialUSB.print(" Yaw: ");
-    SerialUSB.println(euler_angles[2]);
+    MadgwickAHRSupdate(gyro[0], gyro[1], gyro[2], acc[0], acc[1], acc[2], mag[0], mag[1], mag[2]);
+    lastImuTime = millis();
+
+    mavlink_msg_attitude_quaternion_pack(1, MAV_COMP_ID_IMU, &msg, millis(), q0, q1, q2, q3, gyro[0], gyro[1], gyro[2]);
+    msgBuffer.emplace(msg);
+
+    float q[] = {q0, q1, q2, q3};
+    mavlink_quaternion_to_euler(q, &euler_angles[0], &euler_angles[1], &euler_angles[2]);
+    
+
+    if(!enableMotionCal){
+      SerialUSB.print("Roll: ");
+      SerialUSB.print(euler_angles[0]);
+      SerialUSB.print(" Pitch: ");
+      SerialUSB.print(euler_angles[1]);
+      SerialUSB.print(" Yaw: ");
+      SerialUSB.println(euler_angles[2]);
+    } else{
+      //MotionCal
+      SerialUSB.print("Raw:");
+      SerialUSB.print(int(acc[0]*8192/9.8));
+      SerialUSB.print(',');
+      SerialUSB.print(int(acc[1]*8192/9.8));
+      SerialUSB.print(',');
+      SerialUSB.print(int(acc[2]*8192/9.8));
+      SerialUSB.print(',');
+      SerialUSB.print(int(gyro[0] * 16 * radsToDegs));
+      SerialUSB.print(',');
+      SerialUSB.print(int(gyro[1] * 16 * radsToDegs));
+      SerialUSB.print(',');
+      SerialUSB.print(int(gyro[2] * 16 * radsToDegs));
+      SerialUSB.print(',');
+      SerialUSB.print(int(mag[0] * 10));
+      SerialUSB.print(',');
+      SerialUSB.print(int(mag[1] * 10));
+      SerialUSB.print(',');
+      SerialUSB.print(int(mag[2] * 10));
+      SerialUSB.println();
+    }
+    
   }  
 
-  if(millis() - lastHb > 1000){ //Send a hb every second
+  if(millis() - lastHb > 500){ //Send a hb every second
     mavlink_msg_heartbeat_pack(1, MAV_COMP_ID_IMU, &msg, 0,0,0,0,1);
     msgBuffer.emplace(msg);
     
--- src/state_estimator.h
+++
@@ -1,7 +0,0 @@
-#include <Arduino.h>
-
-void UpdateState(float *a, float *g, float *m, float *q, float delta_time, float *euler_angles);
-
-// TODO: remove these for inline functions?
-void MahonyQuaternionUpdate(float *a, float *g, float *m, float *q, float delta_time);
-void MadgwickQuaternionUpdate(float *a, float *g, float *m, float *q, float delta_time);
